d6f11a8d70cd2b2c2da651cf23dd7aaa19d515de
wll_cyg@126.com <wanliang123>
2024-09-13 18:09:41 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Header from "@/component/Header.tsx";
+
+function App() {
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Header.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'antd';
+const Header = () => {
+  return (
+    <div>
+      <Button type='primary'> this is editButton </Button>
+    </div>
+  );
+};
+
+export default Header;
